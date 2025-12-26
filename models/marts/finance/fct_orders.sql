120c35bb2f3d08da6cfdd161488a63bb4aa814c9
Yitian Wang <yitianwang1991@gmail.com>
2025-12-26 20:34:28 +0000

    added documentations to models

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -9,7 +9,7 @@ payments as (
 order_payments as (
     select 
         order_id,
-        sum(case when status = 'success' then amount end) as amount
+        sum(case when ordedr_status = 'success' then amount end) as amount
     from payments
     group by 1
 ),
